b74b594af738f05eb7c7a157d77e9169356fd8c8
Timothy King <tim.king.008@gmail.com>
2023-02-25 23:47:52 +1300

    Fetch product count every 30 seconds on homepage

--- ClientApp/homepage.ts
+++ ClientApp/homepage.ts
@@ -1,1 +1,17 @@
-// TODO: Every 30 seconds fetch a count of the products and then update the HTML
+// Every 30 seconds fetch a count of the products and then update the HTML
+// Notes:
+//     This updates one element with the id product-count - does not handle multiple elements.
+//     An alternative would be to do this in-component, doing the fetch in a useEffect in parent and passing count through
+setInterval(
+  () =>
+    fetch("/Products/Count")
+      .then((response) => response.json())
+      .then((productCount) => {
+        const productCountElement = document.getElementById("product-count");
+
+        if (productCountElement) {
+          productCountElement.innerHTML = `${productCount}`;
+        }
+      }),
+  30000
+);
